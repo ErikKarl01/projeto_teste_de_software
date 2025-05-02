7d2486e5f4e6b7ed8c5bd8952c31deb3ab9d9824
unknown <leandrotab3@gmail.com>
2025-05-02 17:39:30 -0300

    feat: criando a TAD da calculadora com funcao para calculo de variância amostral

---
+++ calculadora.h
@@ -0,0 +1,7 @@
+#ifndef CALCULADORA_H
+#define CALCULADORA_H
+
+// Realiza cálculo da variância da amostra populacional e retorna um valor correspondente
+float calcula_variancia_amostral(float numeros[], int tamanho);
+    
+#endif

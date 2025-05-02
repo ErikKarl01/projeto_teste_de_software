ab533afeb1b5309d37d5694b3814a6e66eecb151
curso_git <kekinpraxedes@gmail.com>
2025-05-02 18:15:30 -0300

    Adição da função desvio_padrao_amostral

---
+++ estatisticaBasica.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+
+double desvio_padrao_amostral(double lista[], int tamanho, double media){
+    double acumulador = 0;
+
+    for (int i = 0; i != tamanho; i++)
+    {
+        acumulador += pow((lista[i] - media), 2);
+    }   
+
+    return sqrt(acumulador/(tamanho - 1));
+
+}

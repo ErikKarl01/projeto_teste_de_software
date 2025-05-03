e09c025dc6e9e1f1c2b7b7dad293736ac7630cb9
curso_git <kekinpraxedes@gmail.com>
2025-05-03 15:40:20 -0300

    teste

--- estatisticaBasica.c
+++ estatisticaBasica.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<math.h>
 
-double desvio_padrao_amostral(double lista[], int tamanho, double media){
+double desvio_padrao_amostral(double lista[], int tamanho, double media, ){
     double acumulador = 0;
 
     for (int i = 0; i != tamanho; i++)

9404eaebbaa61a66cec5aef360418b2a12385a1a
Guiulhas <thechaoszinho@gmail.com>
2025-05-03 10:36:47 -0300

    Adição da função Moda

--- estatisticaBasica.c
+++ estatisticaBasica.c
@@ -12,3 +12,24 @@ double desvio_padrao_amostral(double lista[], int tamanho, double media){
     return sqrt(acumulador/(tamanho - 1));
 
 }
+
+double moda(double lista[], int tamanho) {
+    int max_freq = 0;
+    double moda_valor = lista[0];
+
+    for (int i = 0; i < tamanho; i++) {
+        int freq = 0;
+        for (int j = 0; j < tamanho; j++) {
+            if (lista[i] == lista[j]) {
+                freq++;
+            }
+        }
+
+        if (freq > max_freq) {
+            max_freq = freq;
+            moda_valor = lista[i];
+        }
+    }
+
+    return moda_valor;
+}

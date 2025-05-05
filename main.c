f1af28c1bc0daa0d71050a29d16953c56834322e
LisboaOliveira <124401762+LisboaOliveira@users.noreply.github.com>
2025-05-05 13:42:34 -0300

    mudanca de int para float

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 void verifica_entrada(char entrata[], int tamanho, int *escolha){
     fgets(entrata, tamanho, stdin);
--- media_simples.c
+++ media_simples.c
@@ -1,5 +1,5 @@
-
-int media(int num1, int num2, int num3) {
+//Media Simples
+float media(float num1, float num2, float num3) {
     float num1, num2, num3, media;
 
     media = (num1 + num2 + num3) / 3;

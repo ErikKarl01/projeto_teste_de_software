4527abebc2c2b14784fd2fef35d2ef67319f6068
curso_git <kekinpraxedes@gmail.com>
2025-05-07 19:09:39 -0300

    Adição de funcionalidades

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int verifica_entrada(char entrata[], int tamanho, int *escolha){
     fgets(entrata, tamanho, stdin);
@@ -11,22 +12,80 @@ int verifica_entrada(char entrata[], int tamanho, int *escolha){
     return 1;
 }
 
-void estBasicaMenu(){
-    int escolha_basica;
-    double *lista = NULL;
+void opcao_est_basica(int escolha){
+    double *resultado = 0;
     int tamanho = 0;
+    resultado = restona_list_vallores(&tamanho);
+    switch (escolha)
+    {
+    case 1:
+        
+        break;
 
-    printf("Escolha 1 para calcular média, 2 para mediana, 3 para mode, 4 para variância amostral e 5 para desvio padrão ou 0 para dair deste menu!");
-    scanf("%d", escolha_basica);
+    case 2:
+        /* code */
+        break;
+
+    case 3:
+        /* code */
+        break;
+
+    case 4:
+        /* code */
+        break;
+
+    case 5:
+        /* code */
+        break;
+        
+    default:
+        break;
+    }
+    printf("%f", resultado);
+}
+
+double* restona_list_vallores(int *tamanho){
+    int continuar = 1;
+    double valor;
+    double *lista = NULL;
 
     printf("Adicione valores: ");
-    scanf("");
 
-    while ()
+    while (continuar)
     {
-        int *temp = relloc
+        scanf("%f", &valor);
+
+        int *temp = relloc(lista, (*tamanho + 1)*sizeof(double));
+        if (temp == NULL)
+        {
+            printf("Erro de alocação");
+            free(temp);
+        }
+
+        lista = temp;
+        lista[*tamanho] = valor;
+        tamanho++;
+
+        printf("Deseja continuar? 0 para não e 1 para sim");
+        scanf("%d", continuar);
+    }
+
+    return lista;
+}
+
+void estBasicaMenu(){
+    char entrada_basica[100];
+    int escolha = 0;
+
+    printf("Escolha 1 para calcular média, 2 para mediana, 3 para mode, 4 para variância amostral e 5 para desvio padrão!");
+
+    while (verifica_entrada(entrada_basica, sizeof(entrada_basica), &escolha)){}
+    if (escolha == 0)
+    {
+        return;
     }
     
+    opcao_est_basica(escolha);
 }
 
 void estInferencialMenu();

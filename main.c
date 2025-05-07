2ead5dcc171095a785541bcababb6453e080dae0
curso_git <kekinpraxedes@gmail.com>
2025-05-07 13:34:44 -0300

    Adição de funções a main

--- main.c
+++ main.c
@@ -1,29 +1,40 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-void verifica_entrada(char entrata[], int tamanho, int *escolha){
+int verifica_entrada(char entrata[], int tamanho, int *escolha){
     fgets(entrata, tamanho, stdin);
 
     if (sscanf(entrata, "%d", escolha) != 1)
     {
         printf("Erro ao escolher valor numérico");
-        return;
+        return 0;
     }
+    return 1;
 }
 
-void estBasicaMenu();
+void estBasicaMenu(){
+    int escolha_basica;
+    double *lista = NULL;
+    int tamanho = 0;
+
+    printf("Escolha 1 para calcular média, 2 para mediana, 3 para mode, 4 para variância amostral e 5 para desvio padrão ou 0 para dair deste menu!");
+    scanf("%d", escolha_basica);
+
+    printf("Adicione valores: ");
+    scanf("");
+
+    while ()
+    {
+        int *temp = relloc
+    }
+    
+}
+
 void estInferencialMenu();
 void estProbabilisticaMenu();
 
-void menu_area(int *encerrar){
-    int escolha = 0;
-    char entrata[100];
-
-    printf("Digite 1 para estatística básica, 2 para inferencial, 3 para probabilidade"
-        "e 0 para sair");
-
-    verifica_entrada(entrata, sizeof(entrata), &escolha);
-
-    switch (escolha)
+void opcao_inicial(int opcao_inicial){
+    switch (opcao_inicial)
     {
     case 1:
         estBasicaMenu();
@@ -37,14 +48,28 @@ void menu_area(int *encerrar){
         estProbabilisticaMenu();
         break;
 
-    case 0:
-        *encerrar = 0;
-        break;
-
     default:
         printf("Opção incorreta!");
         break;
     }
+}
+
+void menu_area(int *encerrar){
+    int escolha = 0;
+    char entrata[100];
+
+    printf("Digite 1 para estatística básica, 2 para inferencial, 3 para probabilidade"
+        "e 0 para sair");
+
+
+    if (verifica_entrada(entrata, sizeof(entrata), &escolha) && escolha != 0)
+    {
+        opcao_inicial(escolha);
+    }
+    if (escolha == 0)
+    {
+        *encerrar = 0;
+    }   
 }
 
 

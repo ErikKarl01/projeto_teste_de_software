4e977e9db8b342a3639dd04390de8b59189d5f88
LisboaOliveira <124401762+LisboaOliveira@users.noreply.github.com>
2025-06-03 14:39:21 -0300

    feat: media_simples

--- projeto_final/EstatisticaBasica.java
+++ projeto_final/EstatisticaBasica.java
@@ -1,5 +1,15 @@
 package projeto_final;
 
 public class EstatisticaBasica {
-    
+    double[] entradas;
+
+    double media_simples(){
+        double soma = 0;
+
+        for(double num: this.entradas){
+            soma += num;
+        }
+
+        return soma/this.entradas.length;
+    }
 }

a7ee7b6ef73d0cc096bb83edf2fb9e9091f1290c
curso_git <kekinpraxedes@gmail.com>
2025-06-03 21:38:42 -0300

    feat: adiciona quick sort e ordenacaoParcial

--- projeto_final/EstatisticaBasica.java
+++ projeto_final/EstatisticaBasica.java
@@ -3,7 +3,7 @@ package projeto_final;
 public class EstatisticaBasica {
     double[] entradas;
 
-    double media_simples(){
+    public double media_simples(){
         double soma = 0;
 
         for(double num: this.entradas){
@@ -11,5 +11,34 @@ public class EstatisticaBasica {
         }
 
         return soma/this.entradas.length;
+    }
+
+    public int ordenacaoParcial(double[] array, int inicio, int fim){
+        double pivo = array[fim];
+        int posicao_i = inicio - 1; 
+
+        for (int index = 0; index < fim; index++) {
+            if (array[index] < pivo) {
+                posicao_i++;
+
+                double temporario = array[index];
+                array[index] = array[posicao_i];
+                array[posicao_i] = temporario;
+            }
+        }
+
+        double temporario = array[fim];
+        array[fim] = array[posicao_i + 1];
+        array[posicao_i + 1] = temporario;
+
+        return posicao_i + 1;
+    }
+
+    public void quickSort(double[] array, int inicio, int fim){
+        if (inicio<fim) {
+            int posicao_pivo = ordenacaoParcial(array, inicio, fim);
+            quickSort(array, posicao_pivo + 1, fim);
+            quickSort(array, inicio, posicao_pivo - 1);   
+        }
     }
 }

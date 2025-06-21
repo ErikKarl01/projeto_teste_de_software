c17842ae53c4f7f23039f220dd358818b8ba3e71
curso_git <kekinpraxedes@gmail.com>
2025-06-20 23:23:24 -0300

    feat: adição de funções de freqência simples e acumulada, além da função de calcula da mediana, valor máximo e minimo e desvio padrão

--- projeto_final/EstatisticaBasica.java
+++ projeto_final/EstatisticaBasica.java
@@ -1,5 +1,9 @@
 package projeto_final;
 
+import java.math.*;
+import java.util.HashMap;
+import java.util.Map;
+
 public class EstatisticaBasica {
     double[] entradas;
 
@@ -42,7 +46,23 @@ public class EstatisticaBasica {
         }
     }
 
-    public double variancia_amostral() {
+    public double calculaMediana(){
+        if(entradas == null){
+            System.err.println("Sem valores na lista!");
+            return 0;
+        }
+
+        if (this.entradas.length%2 != 0) {
+            int index = entradas.length/2;
+            return entradas[index];
+        }else{
+            int index_sup = entradas.length/2;
+            int index_inf = entradas.length/2 - 1;
+            return (entradas[index_sup] + entradas[index_inf])/2;
+        }
+    }
+
+    public double calculaVarianciaAmostral() {
         double soma_das_diferencas = 0;
         double media = media_simples();
         for(double num: this.entradas) {
@@ -52,25 +72,59 @@ public class EstatisticaBasica {
         return soma_das_diferencas/(this.entradas.length - 1);
     }
 
+    public double calculaDesvioPadrão(){
+        return Math.sqrt(calculaVarianciaAmostral());
+    }
+
+    public double calculaValorMin(){
+        return this.entradas[0];
+    }
+
+    public double calculaValorMax(){
+        return this.entradas[entradas.length];
+    }
+
+    public Map<Double, Integer> calculaFrequenciaSimples(){
+        Map<Double, Integer> mapa = new HashMap<>();
+
+        for (int index = 0; index < entradas.length; index++) {
+            double chave = entradas[index];
+            mapa.put(chave, mapa.getOrDefault(chave, 0)+1); 
+        }
+
+        return mapa;
+    }
+
+    public Map<Double, Integer> calculaFrequenciaAcumulada(){
+        Map<Double, Integer> mapa = calculaFrequenciaSimples();
+        int soma = 0;
+
+        for (Map.Entry<Double, Integer> entry : mapa.entrySet()) {
+            soma += entry.getValue();
+            mapa.put(entry.getKey(), soma);
+        }
+        return mapa;
+    }
+
     public double calcularModa() {
-    double moda = entradas[0];
-    int maxFrequencia = 1;
+        double moda = entradas[0];
+        int maxFrequencia = 1;
 
-    for (int i = 0; i < entradas.length; i++) {
-        int frequenciaAtual = 1;
+        for (int i = 0; i < entradas.length; i++) {
+            int frequenciaAtual = 1;
 
-        for (int j = i + 1; j < entradas.length; j++) {
-            if (entradas[i] == entradas[j]) {
-                frequenciaAtual++;
+            for (int j = i + 1; j < entradas.length; j++) {
+                if (entradas[i] == entradas[j]) {
+                    frequenciaAtual++;
+                }
+            }
+
+            if (frequenciaAtual > maxFrequencia) {
+                maxFrequencia = frequenciaAtual;
+                moda = entradas[i];
             }
         }
 
-        if (frequenciaAtual > maxFrequencia) {
-            maxFrequencia = frequenciaAtual;
-            moda = entradas[i];
-        }
+        return moda;
     }
-
-    return moda;
-}
 }

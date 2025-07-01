3e25c0978148c21cd002edfe67b6823ccc7567c6
Luiz felipe <felipegordinhogostoso.feras@gmail.com>
2025-07-01 14:59:31 -0300

    feat:criaçao de classe tela de distribuiçao normal

---
+++ projeto_final/TelaDistNormal.java
@@ -0,0 +1,11 @@
+class TelaDistNormal extends JFrame{
+    public TelaDistNormal(){
+        setTitle("Tela de Distribuição Normal");
+        setSize(1000, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        JPanel panel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+
+    }
+}

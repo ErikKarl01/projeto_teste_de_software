72ca04fe2d7c5a1f907b205ed55f62c29cf6954e
curso_git <kekinpraxedes@gmail.com>
2025-07-15 20:30:55 -0300

    refeat: Mudanças estéticas na tela de probabilidade binomial

--- projeto_final/TelaProbBinomial.java
+++ projeto_final/TelaProbBinomial.java
@@ -1,8 +1,10 @@
 package projeto_final;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import javax.swing.*;
+import java.awt.Color;
 
 public class TelaProbBinomial extends JFrame{
     public TelaProbBinomial(){
@@ -12,9 +14,20 @@ public class TelaProbBinomial extends JFrame{
         setLocationRelativeTo(null);
 
         JPanel painel = new JPanel(new GridBagLayout());
+        painel.setBackground(new Color(220, 240, 235));
         GridBagConstraints gbc = new GridBagConstraints();
 
+        //Label de média
         gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new java.awt.Insets(0, 0, 0, 10);
+        JLabel textomediLabel = new JLabel("<html>Insira valor de média: </html>");
+        textomediLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        painel.add(textomediLabel, gbc);
+
+        //TextField de média
+        gbc.gridx = 1;
         gbc.gridy = 0;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.insets = new java.awt.Insets(10, 0, 0, 0);
@@ -22,7 +35,17 @@ public class TelaProbBinomial extends JFrame{
         variavelXField.setPreferredSize(new Dimension(100, 30));
         painel.add(variavelXField, gbc);
 
+        //Label de repetiçoes
         gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new java.awt.Insets(0, 0, 0, 10);
+        JLabel textoRepeticoesJLabel = new JLabel("<html>Insira valor de média: </html>");
+        textoRepeticoesJLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        painel.add(textoRepeticoesJLabel, gbc);
+
+        //TextField de repetições
+        gbc.gridx = 1;
         gbc.gridy = 1;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.insets = new java.awt.Insets(10, 0, 0, 0);
@@ -30,7 +53,17 @@ public class TelaProbBinomial extends JFrame{
         mediaField.setPreferredSize(new Dimension(100, 30));
         painel.add(mediaField, gbc);
 
+        //Label de sucessos
         gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new java.awt.Insets(0, 0, 0, 10);
+        JLabel textoSucessosJLabel = new JLabel("<html>Insira valor de média: </html>");
+        textoSucessosJLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        painel.add(textoSucessosJLabel, gbc);
+
+        //TextField de sucessos
+        gbc.gridx = 1;
         gbc.gridy = 2;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.insets = new java.awt.Insets(10, 0, 0, 0);
@@ -38,14 +71,17 @@ public class TelaProbBinomial extends JFrame{
         desvioPadraoField.setPreferredSize(new Dimension(100, 30));
         painel.add(desvioPadraoField, gbc);
 
+        //Label de saída
         gbc.gridx = 0;
         gbc.gridy = 3;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.insets = new java.awt.Insets(10, 0, 0, 0);
-        JTextField saidaTextField = new JTextField();
-        saidaTextField.setPreferredSize(new Dimension(100, 30));
+        JLabel saidaTextField = new JLabel();
+        saidaTextField.setFont(new Font("Arial", Font.BOLD, 16));
+        saidaTextField.setPreferredSize(new Dimension(250, 80));
         painel.add(saidaTextField, gbc);
 
+        //Botão de calcular
         gbc.gridx = 0;
         gbc.gridy = 4;
         gbc.fill = GridBagConstraints.HORIZONTAL;
@@ -60,40 +96,41 @@ public class TelaProbBinomial extends JFrame{
             try {
                 probabilidade = Double.parseDouble(variavelXField.getText().trim());
             } catch (NumberFormatException ex) {
-                saidaTextField.setText("ERRO: Valor não numérico detectado em probabilidade");
+                saidaTextField.setText("<html> Valor não numérico detectado em probabilidade</html>");
                 return;
             }
             try {
                 repeticoes = Integer.parseInt(desvioPadraoField.getText().trim());
             } catch (NumberFormatException ex) {
-                saidaTextField.setText("ERRO: Valor não numérico detectado em repetições");
+                saidaTextField.setText("<html>Valor não numérico detectado em repetições</html>");
                 return;
             }
             try {
                 sucessos = Integer.parseInt(mediaField.getText().trim());
             } catch (NumberFormatException ex) {
-                saidaTextField.setText("ERRO: Valor não numérico detectado em sucessos");
+                saidaTextField.setText("<html>Valor não numérico detectado em sucessos</html>");
                 return;
             }
 
             if (probabilidade != null && repeticoes !=  null && sucessos != null) {
                 if (probabilidade < 0 || probabilidade > 1) {
-                    saidaTextField.setText("Probabilidade deve estar entre 0 e 1");
+                    saidaTextField.setText("<html>Probabilidade deve estar entre 0 e 1</html>");
                 }else if (repeticoes <= 0){
-                    saidaTextField.setText("Repetições não podem ser negativas");
+                    saidaTextField.setText("<html>Repetições não podem ser negativas</html>");
                 }else if (sucessos < 0){
-                    saidaTextField.setText("Sucessos não podem ser negativos");
+                    saidaTextField.setText("<html>Sucessos não podem ser negativos</html>");
                 } else if (sucessos > repeticoes) {
-                    saidaTextField.setText("Sucessos não podem ser maiores que repetições");
+                    saidaTextField.setText("<html>Sucessos não podem ser maiores que repetições</html>");
                 } else if (sucessos > 100 || repeticoes > 100) {
-                    saidaTextField.setText("Valores muito grandes detectados entradas repetições, máximo 100");
+                    saidaTextField.setText("<html>Valores muito grandes detectados entradas repetições, máximo 100</html>");
                 } else {
-                    saidaTextField.setText("Resultado: " + Probabilidade.calcularProbabilidadeBinomial(probabilidade, repeticoes, sucessos));
+                    saidaTextField.setText("<html>Resultado: " + Probabilidade.calcularProbabilidadeBinomial(probabilidade, repeticoes, sucessos) + "</html>");
                 }
             }
         });
         painel.add(calcularButton, gbc);
 
+        //Botão de voltar
         gbc.gridx = 0;
         gbc.gridy = 5;
         gbc.fill = GridBagConstraints.HORIZONTAL;

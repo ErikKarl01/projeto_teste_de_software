ec678b6aac7b3ed28a64455bfdb2475aca5dce3d
curso_git <kekinpraxedes@gmail.com>
2025-07-26 14:08:19 -0300

    Feat: Tratativa das variáveis e inserção de design na tela de proporção simples

--- projeto_final/TesteProporcaoSimples.java
+++ projeto_final/TesteProporcaoSimples.java
@@ -1,6 +1,10 @@
 package projeto_final;
+
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import javax.swing.*;
 
 public class TesteProporcaoSimples extends JFrame{
@@ -11,36 +15,72 @@ public class TesteProporcaoSimples extends JFrame{
         setLocationRelativeTo(null);
 
         JPanel painel = new JPanel(new GridBagLayout());
+        painel.setBackground(new Color(210, 180, 140));
         GridBagConstraints gbc = new GridBagConstraints();
 
+        // Proporção amostral label
         gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new java.awt.Insets(0, 0, 0, 10);
+        JLabel proporcaoAmostralLabel = new JLabel("<html>Proporção amostral: </html>");
+        proporcaoAmostralLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        painel.add(proporcaoAmostralLabel, gbc);
+
+        // Proporção amostral text field
+        gbc.gridx = 1;
         gbc.gridy = 0;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         JTextField proposcaoAmostral = new JTextField();
         painel.add(proposcaoAmostral, gbc);
 
+        // Proposição hipotética label
         gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new java.awt.Insets(0, 0, 0,10);
+        JLabel proporcaoHipoteticaLabel = new JLabel("<html>Proposição hipotética: </html>");
+        proporcaoHipoteticaLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        painel.add(proporcaoHipoteticaLabel, gbc);
+
+        // proposção hipotetica text field
+        gbc.gridx = 1;
         gbc.gridy = 1;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         JTextField proporcaoHipotetica = new JTextField();
         painel.add(proporcaoHipotetica, gbc);
 
+        // Tamanho da amostra label
         gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new java.awt.Insets(0, 0, 0, 10);
+        JLabel tamanhoAmostraLabel = new JLabel("<html>Tamanho da amostra: </html>");
+        tamanhoAmostraLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        painel.add(tamanhoAmostraLabel, gbc);
+
+        // tamanho amostra text field
+        gbc.gridx = 1;
         gbc.gridy = 2;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         JTextField tamanhoAmostra = new JTextField();
         painel.add(tamanhoAmostra, gbc);
 
-        gbc.gridx = 0;
+        // Saída text field
+        gbc.gridx = 1;
         gbc.gridy = 3;
         gbc.fill = GridBagConstraints.HORIZONTAL;
-        JTextField saidaTextField = new JTextField();
+        JTextField saidaTextField = new JTextField(15);
         painel.add(saidaTextField, gbc);
 
+        // botão calcular
         gbc.gridx = 0;
         gbc.gridy = 4;
         gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new java.awt.Insets(0, 0, 10, 0);
         JButton calcularButton = new JButton();
+        calcularButton.setText("Calcular");
+        calcularButton.setPreferredSize(new Dimension(100, 50));
         calcularButton.addActionListener(e -> {
             Double proporcaoAmostraDouble = null;
             Double proposcaoHipoteticaDouble = null;
@@ -63,19 +103,27 @@ public class TesteProporcaoSimples extends JFrame{
             }
 
             if (proporcaoAmostraDouble != null && proposcaoHipoteticaDouble !=  null && tamanhoAmostraInteger != null) {
-                if (tamanhoAmostraInteger <= 0 || proposcaoHipoteticaDouble <= 0 || proposcaoHipoteticaDouble >= 1) {
-                    saidaTextField.setText("Entradas inválidas");
-                } else{
+                if (proporcaoAmostraDouble < 0 || proporcaoAmostraDouble > 1) {
+                    saidaTextField.setText("ERRO: Proporção amostral deve estar entre 0 e 1");
+                } else if (proposcaoHipoteticaDouble < 0 || proposcaoHipoteticaDouble > 1) {
+                    saidaTextField.setText("ERRO: Proposição hipotética deve estar entre 0 e 1");
+                } else if (tamanhoAmostraInteger <= 0 || tamanhoAmostraInteger > 1000000000) {
+                    saidaTextField.setText("ERRO: Tamanho da amostra deve ser maior que zero e menor que 1.000.000.000");
+                } else {
                     saidaTextField.setText("Resultado: " + EstatisticaInferencial.testeProporcaoSimples(proporcaoAmostraDouble, proposcaoHipoteticaDouble, tamanhoAmostraInteger));
                 }
             }
         });
         painel.add(calcularButton, gbc);
 
+        // botão voltar
         gbc.gridx = 0;
         gbc.gridy = 5;
         gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new java.awt.Insets(0, 0, 10, 0);
         JButton voltarButton = new JButton();
+        voltarButton.setText("Voltar");
+        voltarButton.setPreferredSize(new Dimension(100, 50));
         voltarButton.addActionListener(e -> {
             TelaProbabilidade telaProbabilidade = new TelaProbabilidade();
             telaProbabilidade.setVisible(true);
